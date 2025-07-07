4bca7bbf643b3e544e2fded339402f47037c42fb
sunshinetoyou <victorr0618@gmail.com>
2025-07-07 18:49:26 +0900

    별점 기능 및 결과 페이지 추가

--- polls/urls.py
+++ polls/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.project_list, name='project_list'),
-    path('detail/<int:pk>', views.project_detail, name='project_detail')
+    path('detail/<int:pk>', views.project_detail, name='project_detail'),
+    path('result/<int:pk>', views.project_result, name='project_result')
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
+from django.db.models import Avg
 from .models import Project, Vote
 
 def project_list(req):
@@ -9,5 +10,21 @@ def project_list(req):
 def project_detail(req, pk):
     project = get_object_or_404(Project, pk=pk)
     context = {'project': project}
+
+    if req.method == 'POST':
+        score = int(req.POST.get('score'))
+        Vote.objects.create(project=project, score=score)
+        return redirect('project_result', pk=pk)
     
     return render(req, 'polls/project_detail.html', context)
+
+def project_result(request, pk):
+    project = get_object_or_404(Project, pk=pk)
+    votes = Vote.objects.filter(project=project)
+    avg_score = votes.aggregate(avg=Avg('score'))['avg']
+    vote_count = votes.count()
+    return render(request, 'polls/project_result.html', {
+        'project': project,
+        'avg_score': avg_score,
+        'vote_count': vote_count,
+    })
