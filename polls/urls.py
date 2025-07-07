745a0b6e9acf0859a4a1fce801a3550b985aef44
sunshinetoyou <victorr0618@gmail.com>
2025-07-07 18:38:27 +0900

    detail 페이지 추가

--- polls/urls.py
+++ polls/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.project_list, name='project_list')
+    path('', views.project_list, name='project_list'),
+    path('detail/<int:pk>', views.project_detail, name='project_detail')
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Project, Vote
 
 def project_list(req):
     projects = Project.objects.all()
     context = {'projects': projects}
     return render(req, 'polls/project_list.html', context)
+
+def project_detail(req, pk):
+    project = get_object_or_404(Project, pk=pk)
+    context = {'project': project}
+    
+    return render(req, 'polls/project_detail.html', context)
