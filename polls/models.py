2349a0ff1c41719cdab5081deb809198bcce78c0
sunshinetoyou <victorr0618@gmail.com>
2025-07-07 18:07:55 +0900

    DB(project, vote) 추가

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    subject = models.CharField(max_length=200)  # 제목에 200자 제한
+    detail = models.TextField()
+
+class Vote(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)  # FK 설정
+    score = models.IntegerField(choices=[(i, i) for i in range(1,6)])   # 1~5 사이의 정수값만 입력 받음
+    
